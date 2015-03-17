c21c93acbb93a51fe1b8539d1d7bbf606fa14ae9
JamesKent <jameskent91@gmail.com>
2015-03-16 23:58:04 -0400

    Added Three Way Data Binding

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-//This create our Angular module and injects firebase
+// create our angular module and inject firebase
 angular.module('scheduleApp', ['firebase'])
 
-//Next Create our main controller and get access to firebase
-.controller('mainController', function($scope. $firebase){
-	
-	//application code here
-	
-//Connect to firebase
-	
-	var ref = new Firebase("amber-heat-8977.firebaseio.com/days");
-	var fb = $firebase(ref);
-	
-  $scope.reset = function() {    
+// create our main controller and get access to firebase
+.controller('mainController', function($scope, $firebase) {
+  
+  // connect to firebase 
+  var ref = new Firebase("https://burning-torch-4263.firebaseio.com/days");  
+  var fb = $firebase(ref);
 
-    fb.$set({
-      monday: {
-        name: 'Monday',
-        slots: {
-          0900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          }
-        }
-      },
-      tuesday: {
-        name: 'Tuesday',
-        slots: {
-          0900: {
-            time: '9:00am',
-            booked: false
-          },
-          0110: {
-            time: '11:00am',
-            booked: false
-          }
-        }
-      }
-    });    
+  // sync as object 
+  var syncObject = fb.$asObject();
 
-  };
+  // three way data binding
+  syncObject.$bindTo($scope, 'days');
   
 });
-	
