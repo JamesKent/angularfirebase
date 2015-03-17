61e55a14cfd92e6e46c407f0986db693d496c6d8
JamesKent <jameskent91@gmail.com>
2015-03-16 21:31:35 -0400

    First Commit - Added Code to Angular file

---
+++ app.js
@@ -0,0 +1,9 @@
+//This create our Angular module and injects firebase
+angular.module('scheduleApp', ['firebase'])
+
+//Next Create our main controller and get access to firebase
+.controller('mainController', function($scope. $firebase){
+	
+	//application code here
+	
+	}
